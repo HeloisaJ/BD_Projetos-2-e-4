03c1e55e7fc0b589f2bc3432be7d70485b8be0b9
PedroBenedito1512 <pedro.00000844519@unicap.br>
2024-04-25 15:48:53 -0300

    questão 4 feita

--- DQL.sql
+++ DQL.sql
@@ -8,4 +8,13 @@ group by editora;
 
 -- Questão 3
 select planos_de_pagamento_id, count(*) from define
-group by planos_de_pagamento_id;
+group by planos_de_pagamento_id limit 1;
+
+--Questão 4 
+SELECT ae.nome, ae.custo, COUNT(pe.aluno_num_matricula) AS total_participantes
+FROM atividade_extracurricular ae
+LEFT JOIN participa_extracurrilar pe ON ae.id_atividade = pe.atividade_extracurricular_id
+GROUP BY ae.nome, ae.custo
+ORDER BY total_participantes ASC, ae.custo DESC;
+--se quiser apenas um colocar no final " limit 1" vai aparecer a  
+--atividade extra curricular mais cara e menos popular
