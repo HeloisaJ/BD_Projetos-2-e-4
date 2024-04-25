234c05b411f9453e826fcd6d60644e0a69f837ef
PedroBenedito1512 <pedro.00000844519@unicap.br>
2024-04-25 14:55:53 -0300

    questão 3 feita

--- DQL.sql
+++ DQL.sql
@@ -5,3 +5,7 @@ group by atividade_extracurricular_id;
 -- Questão 2
 select editora, count(*) from livros
 group by editora;
+
+-- Questão 3
+select planos_de_pagamento_id, count(*) from define
+group by planos_de_pagamento_id;
