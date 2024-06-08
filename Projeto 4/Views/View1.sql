8caaa6a1f11ba5499e4f58b04905251572c8146d
HeloisaJ <heloisa.00000845226@unicap.br>
2024-06-08 17:32:45 -0300

    Mudanças nas views, Criação de 2 autorizações  e criação das pastas

--- Projeto 4/Views/View1.sql
+++ Projeto 4/Views/View1.sql
@@ -1,21 +1,21 @@
-CREATE OR REPLACE VIEW totalArrecadadoPorPessoa AS -- FUNCIONA, vê o total arrecadado por pessoa na escola
+CREATE OR REPLACE VIEW total_arrecadado_por_pessoa AS -- NÃO FUNCIONA no MariaDB, vê o total arrecadado por pessoa na escola
 	SELECT
 		p_nome, sobrenome, genero, p_tipo,
 		(SELECT coalesce(SUM(valor_multa),0) FROM emprestimo WHERE pessoa.id_pessoa = emprestimo.id_pessoa) + 
 		(SELECT coalesce(SUM(valor_pag_matricula),0) FROM aluno WHERE pessoa.id_pessoa = aluno.id_pessoa) + 
 		(SELECT COALESCE(sum(total), 0) from (SELECT pe.*,(SELECT(sum(custo))
 						from atividade_extracurricular as ae
-						WHERE pe.atividade_extracurricular_id = ae.id_atividade) as total from participa_extracurrilar as pe JOIN aluno ON pe.aluno_num_matricula = aluno.num_matricula WHERE pessoa.id_pessoa = aluno.id_pessoa)) + 
+						WHERE pe.atividade_extracurricular_id = ae.id_atividade) as total from participa_extracurrilar as pe JOIN aluno ON pe.aluno_num_matricula = aluno.num_matricula WHERE pessoa.id_pessoa = aluno.id_pessoa) AS q1) + 
 		(SELECT COALESCE(sum(total), 0) from (SELECT pref.*, (SELECT(sum(custo))
 						from aula_de_reforco as ar
-						WHERE pref.aula_de_reforco = ar.id_reforco) as total from participa_reforco as pref JOIN aluno ON pref.aluno_num_matricula = aluno.num_matricula WHERE pessoa.id_pessoa = aluno.id_pessoa)) +
+						WHERE pref.aula_de_reforco = ar.id_reforco) as total from participa_reforco as pref JOIN aluno ON pref.aluno_num_matricula = aluno.num_matricula WHERE pessoa.id_pessoa = aluno.id_pessoa) AS q2) +
 		(SELECT COALESCE(sum(total), 0) from (SELECT prefe.*, (SELECT(sum(custo))
 						from preparatorio as pr
-						WHERE prefe.preparatorio_id = pr.id_preparatorio) as total from participa_preparatorio as prefe JOIN aluno ON prefe.aluno_num_matricula = aluno.num_matricula WHERE pessoa.id_pessoa = aluno.id_pessoa)) +
+						WHERE prefe.preparatorio_id = pr.id_preparatorio) as total from participa_preparatorio as prefe JOIN aluno ON prefe.aluno_num_matricula = aluno.num_matricula WHERE pessoa.id_pessoa = aluno.id_pessoa) AS q3) +
 		(SELECT COALESCE(SUM(custo_livro), 0) FROM comprar WHERE pessoa.id_pessoa = comprar.id_pessoa) as receita_bruta
 	FROM pessoa;
 		
-SELECT * FROM totalArrecadadoPorPessoa;
+SELECT * FROM total_arrecadado_por_pessoa;
 
 -- Dúvidas:
 
