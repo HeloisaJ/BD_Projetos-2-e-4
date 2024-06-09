ae1d96dd0985681424f65dcc3032b847c58a4fcf
HeloisaJ <heloisa.00000845226@unicap.br>
2024-06-09 18:45:51 -0300

    Último procedimento

--- Projeto 4/Stored Procedures/Procedure4.sql
+++ Projeto 4/Stored Procedures/Procedure4.sql
@@ -1,1 +1,8 @@
-CREATE PROCEDURE  ()
+CREATE PROCEDURE parcerias_de_uma_secretaria (IN secretariaId INTEGER)
+BEGIN
+    SELECT editora_nome
+    FROM parceria
+    WHERE secretaria_id = secretariaId;
+END;
+
+CALL parcerias_de_uma_secretaria(1);
